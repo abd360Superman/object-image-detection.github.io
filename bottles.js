d39675f785d4e601aaff0b8a30d09a1e4da9bd3b
abd360superman <darshchoudhary171@gmail.com>
2021-07-30 18:44:55 +0530

    Entry 1

---
+++ bottles.js
@@ -0,0 +1,47 @@
+function loadHome() {
+    window.location = 'index.html';
+}
+
+let status = "";
+let img = "";
+let objects = [];
+
+function preload() {
+    img = loadImage("images/bottles.jpg");
+}
+
+function setup() {
+    const canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (status != "") {
+        for (let c = 0; c < objects.length; c++) { 
+            document.getElementById("status").innerHTML = "Status: Object Detected"; 
+            fill("#FF0000"); 
+            let percent = floor(objects[c].confidence * 100); 
+            text(objects[c].label + " " + percent + "%", objects[c].x + 15, objects[c].y + 15); 
+            noFill(); 
+            stroke("#FF0000"); 
+            rect(objects[c].x, objects[c].y, objects[c].width, objects[c].height); 
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+function loadBedroom() {
+    window.location = 'bedroom.html'
+}
+
+function loadLiving() {
+    window.location = 'living.html'
+}
+
+function loadFruits() {
+    window.location = 'fruits.html'
+}
+
+function loadBottles() {
+    window.location = 'bottles.html'
+}
+
+function loadDesk() {
+    window.location = 'desk.html'
+}
